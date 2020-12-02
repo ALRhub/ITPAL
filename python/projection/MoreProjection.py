fixed gradients for split KL projection

python/projection/MoreProjection.py
@@ -1,5 +1,5 @@
 import numpy as np
-from projection.ITPS import ITPS
+from python.projection.ITPS import ITPS
 
 
 class MoreProjection(ITPS):
@@ -96,24 +96,26 @@ class MoreProjection(ITPS):
         assert self._succ, "INVALID STATE, No previous successfull execution!"
 
         deta_dq_target, deta_dQ_target, domega_dq_target, domega_dQ_target = self.get_last_eo_grad()
-        deo_dq_target = np.stack([deta_dq_target, domega_dq_target], axis=0)   #  2 x d
-        deo_dQ_target = np.stack([np.reshape(deta_dQ_target, -1), np.reshape(domega_dQ_target, -1)], axis=0) # 2 x d**2
+        deo_dq_target = np.stack([deta_dq_target, domega_dq_target], axis=0)  # 2 x d
+        deo_dQ_target = np.stack([np.reshape(deta_dQ_target, -1), np.reshape(domega_dQ_target, -1)], axis=0)  # 2 x d**2
 
-        dq_deta = ((self._omega + 1) * self._old_lin - self._target_lin) / ((self._omega + self._eta + 1)**2)
-        dq_domega = -(self._eta * self._old_lin + self._target_lin) / ((self._omega + self._eta + 1)**2)
-        dq_deo = np.stack([dq_deta, dq_domega], axis=1)   # d x 2
+        dq_deta = ((self._omega + 1) * self._old_lin - self._target_lin) / ((self._omega + self._eta + 1) ** 2)
+        dq_domega = -(self._eta * self._old_lin + self._target_lin) / ((self._omega + self._eta + 1) ** 2)
+        dq_deo = np.stack([dq_deta, dq_domega], axis=1)  # d x 2
 
-        dQ_deta = ((self._omega + 1) * self._old_precision - self._target_precision) / ((self._omega + self._eta + 1)**2)
-        dQ_domega = -(self._eta * self._old_precision + self._target_precision) / ((self._omega + self._eta + 1)**2)
-        dQ_deo = np.stack([np.reshape(dQ_deta, -1), np.reshape(dQ_domega, -1)], axis=1)   # d**2 x 2
+        dQ_deta = ((self._omega + 1) * self._old_precision - self._target_precision) / (
+                    (self._omega + self._eta + 1) ** 2)
+        dQ_domega = -(self._eta * self._old_precision + self._target_precision) / ((self._omega + self._eta + 1) ** 2)
+        dQ_deo = np.stack([np.reshape(dQ_deta, -1), np.reshape(dQ_domega, -1)], axis=1)  # d**2 x 2
 
-        t1 = dQ_deo @ deo_dQ_target + np.eye(self._dim**2) / (self._eta + self._omega + 1)
+        t1 = dQ_deo @ deo_dQ_target + np.eye(self._dim ** 2) / (self._eta + self._omega + 1)
         x = np.reshape(t1, [self._dim, self._dim, self._dim, self._dim])
         x = self._proj_precision @ self._target_precision @ x @ self._proj_covar @ self._target_precision
 
-        dq_dqtilde = np.outer(dq_deta, deta_dq_target) + np.outer(dq_domega, domega_dq_target) + np.eye(self._dim) / (self._eta + self._omega +1)
+        dq_dqtilde = np.outer(dq_deta, deta_dq_target) + np.outer(dq_domega, domega_dq_target) + np.eye(self._dim) / (
+                    self._eta + self._omega + 1)
         dmu_dmutilde = self._proj_covar @ dq_dqtilde @ self._target_precision
-        #dmu_dmutilde = self._target_precision @  dq_dqtilde @ self._proj_covar
+        # dmu_dmutilde = self._target_precision @  dq_dqtilde @ self._proj_covar
         return dmu_dmutilde, x
 
     def backward(self, d_mean, d_cov):
@@ -125,9 +127,10 @@ class MoreProjection(ITPS):
         assert self._succ, "INVALID STATE, No previous successfull execution!"
         deta_dq_target, deta_dQ_target, domega_dq_target, domega_dQ_target = self.get_last_eo_grad()
 
-        dq_deta = ((self._omega + 1) * self._old_lin - self._target_lin) / ((self._omega + self._eta + 1)**2)
-        dQ_deta = ((self._omega + 1) * self._old_precision - self._target_precision) / ((self._omega + self._eta + 1)**2)
-        dq_domega = -(self._eta * self._old_lin + self._target_lin) / ((self._omega + self._eta + 1)**2)
+        dq_deta = ((self._omega + 1) * self._old_lin - self._target_lin) / ((self._omega + self._eta + 1) ** 2)
+        dQ_deta = ((self._omega + 1) * self._old_precision - self._target_precision) / (
+                    (self._omega + self._eta + 1) ** 2)
+        dq_domega = -(self._eta * self._old_lin + self._target_lin) / ((self._omega + self._eta + 1) ** 2)
         dQ_domega = -(self._eta * self._old_precision + self._target_precision) / ((self._omega + self._eta + 1) ** 2)
 
         # mean
@@ -154,34 +157,34 @@ class MoreProjection(ITPS):
         For each case (except the first) there is a "baseline" implementation, which should correspond to the
         equations in the overleaf, and a tuned version, optimized to reduce number of performed operations
         """
-        assert self._succ, "INVALID STATE, No previous successfull execution!"
-        if self._eta == 0 and self._omega == 0:    # case 1
+        assert self._succ, "INVALID STATE, No previous successful execution!"
+        if self._eta == 0 and self._omega == 0:  # case 1
             return np.zeros(self._dim), np.zeros([self._dim, self._dim]), \
                    np.zeros(self._dim), np.zeros([self._dim, self._dim])
 
-        elif self._eta == 0 and self._omega > 0:     # case 2
+        elif self._eta == 0 and self._omega > 0:  # case 2
             at, bt, ct, dt = self._case2_tuned()
-            #ab, bb, cb, db = self._case2_baseline()
-            #print(np.max(np.abs(cb - ct)), np.max(np.abs(cb - ct)))
+            # ab, bb, cb, db = self._case2_baseline()
+            # print(np.max(np.abs(cb - ct)), np.max(np.abs(cb - ct)))
             return at, bt, ct, dt
 
-        elif self._eta > 0 and self._omega == 0:     # case 3
+        elif self._eta > 0 and self._omega == 0:  # case 3
             at, bt, ct, dt = self._case3_baseline2()
-            #ab, bb, cb, db = self._case3_baseline()
-            #print(np.max(np.abs(ab - at)), np.max(np.abs(bb - bt)))
+            # ab, bb, cb, db = self._case3_baseline()
+            # print(np.max(np.abs(ab - at)), np.max(np.abs(bb - bt)))
             return at, bt, ct, dt
 
         elif self._eta > 0 and self._omega > 0:
             at, bt, ct, dt = self._case4_tuned()
-            #ab, bb, cb, db = self._case4_baseline()
-            #print(np.max(np.abs(ab - at)), np.max(np.abs(bb - bt)), np.max(np.abs(cb - ct)), np.max(np.abs(db - dt)))
+            # ab, bb, cb, db = self._case4_baseline()
+            # print(np.max(np.abs(ab - at)), np.max(np.abs(bb - bt)), np.max(np.abs(cb - ct)), np.max(np.abs(db - dt)))
             return at, bt, ct, dt
 
         else:
             raise AssertionError("WTF?")
 
     def _case2_baseline(self):
-        dQ_omega = - (self._eta * self._old_precision + self._target_precision) / ((self._omega + self._eta + 1)**2)
+        dQ_omega = - (self._eta * self._old_precision + self._target_precision) / ((self._omega + self._eta + 1) ** 2)
         lhs = np.trace(self._proj_covar @ dQ_omega)
         rhs = - self._proj_covar / (self._omega + self._eta + 1)
         domega_dQ = rhs / lhs
@@ -193,9 +196,10 @@ class MoreProjection(ITPS):
 
     def _case3_baseline2(self):
         dq_deta = ((self._omega + 1) * self._old_lin - self._target_lin) / ((self._omega + self._eta + 1) ** 2)
-        dQ_deta = ((self._omega + 1) * self._old_precision - self._target_precision) / ((self._omega + self._eta + 1) ** 2)
-        dq_domega = - (self._eta * self._old_lin + self._target_lin) / ((self._omega + self._eta + 1))**2
-        dQ_domega = - (self._eta * self._old_precision + self._target_precision) / ((self._omega + self._eta + 1))**2
+        dQ_deta = ((self._omega + 1) * self._old_precision - self._target_precision) / (
+                    (self._omega + self._eta + 1) ** 2)
+        dq_domega = - (self._eta * self._old_lin + self._target_lin) / ((self._omega + self._eta + 1)) ** 2
+        dQ_domega = - (self._eta * self._old_precision + self._target_precision) / ((self._omega + self._eta + 1)) ** 2
 
         dtm1_dq = self._proj_covar @ self._old_lin
         dtm2_dq = 2 * self._proj_covar @ self._old_precision @ self._proj_covar @ self._proj_lin
@@ -210,15 +214,16 @@ class MoreProjection(ITPS):
         f2_dQ = dtlogdet_dQ + dttrace_dQ - 2 * dtm1_dQ + dtm2_dQ
 
         lhs = np.dot(f2_dq, dq_deta) + np.trace(f2_dQ @ dQ_deta)
-        rhs_dq = -f2_dq / (self._omega + self._eta + 1) #- dq_deta - dq_domega
-        rhs_dQ = -f2_dQ / (self._omega + self._eta + 1) #- dQ_deta - dQ_domega
+        rhs_dq = -f2_dq / (self._omega + self._eta + 1)  # - dq_deta - dq_domega
+        rhs_dQ = -f2_dQ / (self._omega + self._eta + 1)  # - dQ_deta - dQ_domega
         deta_dq = rhs_dq / lhs
         deta_dQ = rhs_dQ / lhs
         return deta_dq, deta_dQ, np.zeros(self._dim), np.zeros([self._dim, self._dim])
 
     def _case3_baseline(self):
         dq_deta = ((self._omega + 1) * self._old_lin - self._target_lin) / ((self._omega + self._eta + 1) ** 2)
-        dQ_deta = ((self._omega + 1) * self._old_precision - self._target_precision) / ((self._omega + self._eta + 1) ** 2)
+        dQ_deta = ((self._omega + 1) * self._old_precision - self._target_precision) / (
+                    (self._omega + self._eta + 1) ** 2)
 
         dtm1_dq = self._proj_covar @ self._old_lin
         dtm2_dq = 2 * self._proj_covar @ self._old_precision @ self._proj_covar @ self._proj_lin
@@ -257,10 +262,11 @@ class MoreProjection(ITPS):
         return deta_dq, deta_dQ, np.zeros(self._dim), np.zeros([self._dim, self._dim])
 
     def _case4_baseline(self):
-        dq_deta = ((self._omega + 1) * self._old_lin - self._target_lin) / ((self._omega + self._eta + 1))**2
-        dQ_deta = ((self._omega + 1) * self._old_precision - self._target_precision) / ((self._omega + self._eta + 1))**2
-        dq_domega = - (self._eta * self._old_lin + self._target_lin) / ((self._omega + self._eta + 1))**2
-        dQ_domega = - (self._eta * self._old_precision + self._target_precision) / ((self._omega + self._eta + 1))**2
+        dq_deta = ((self._omega + 1) * self._old_lin - self._target_lin) / ((self._omega + self._eta + 1)) ** 2
+        dQ_deta = ((self._omega + 1) * self._old_precision - self._target_precision) / (
+        (self._omega + self._eta + 1)) ** 2
+        dq_domega = - (self._eta * self._old_lin + self._target_lin) / ((self._omega + self._eta + 1)) ** 2
+        dQ_domega = - (self._eta * self._old_precision + self._target_precision) / ((self._omega + self._eta + 1)) ** 2
 
         dtm1_dq = self._proj_covar @ self._old_lin
         dtm2_dq = 2 * self._proj_covar @ self._old_precision @ self._proj_covar @ self._proj_lin
@@ -287,13 +293,16 @@ class MoreProjection(ITPS):
         deta_dq = rhs_q / lhs_q_eta
         domega_dq = rhs_q / lhs_q_omega
 
-        rhs_Q_eta2 = (1 / np.trace(dtlogdet_dQ @ dQ_domega)) * (np.dot(f2_dq, dq_domega) + np.trace(f2_dQ @ dQ_domega)) * dtlogdet_dQ / (self._omega + self._eta + 1)
+        rhs_Q_eta2 = (1 / np.trace(dtlogdet_dQ @ dQ_domega)) * (
+                    np.dot(f2_dq, dq_domega) + np.trace(f2_dQ @ dQ_domega)) * dtlogdet_dQ / (
+                                 self._omega + self._eta + 1)
         rhs_Q_eta1 = - f2_dQ / (self._omega + self._eta + 1)
         rhs_Q_eta = rhs_Q_eta1 + rhs_Q_eta2
 
         deta_d_Q = rhs_Q_eta / lhs_q_eta
 
-        rhs_Q_omega2 = (1 / np.trace(dtlogdet_dQ @ dQ_deta)) * (np.dot(f2_dq, dq_deta) + np.trace(f2_dQ @ dQ_deta)) * dtlogdet_dQ / (self._omega + self._eta + 1)
+        rhs_Q_omega2 = (1 / np.trace(dtlogdet_dQ @ dQ_deta)) * (
+                    np.dot(f2_dq, dq_deta) + np.trace(f2_dQ @ dQ_deta)) * dtlogdet_dQ / (self._omega + self._eta + 1)
         rhs_Q_omega1 = - f2_dQ / (self._omega + self._eta + 1)
         rhs_Q_omega = rhs_Q_omega1 + rhs_Q_omega2
         domega_d_Q = rhs_Q_omega / lhs_q_omega
@@ -337,6 +346,3 @@ class MoreProjection(ITPS):
         domega_d_Q = (f2_deta * self._proj_covar / tr_dlogdet_deta - f2_dQ) / lhs_q_omega
 
         return deta_dq, deta_d_Q, domega_dq, domega_d_Q
-
-
-

python/projection/SplitMoreProjection.py
@@ -46,7 +46,7 @@ class SplitMoreProjection(object):
         self._succ = True
 
         self._old_mean = old_mean
-        self._old_precision = np.linalg.inv(old_covar)
+        self._old_precision = np.linalg.solve(old_covar, np.eye(self._dim))
         self._old_lin = self._old_precision @ self._old_mean
 
         self._old_chol_precision_t = np.linalg.cholesky(self._old_precision).T
@@ -63,6 +63,8 @@ class SplitMoreProjection(object):
             opt_eta_mu = self.opt_dual(self._dual_mean)
             self._eta_mu = opt_eta_mu
             self._proj_mean = self._new_mean(opt_eta_mu)
+            self._Q_mu = (self._eta_mu * self._old_precision + self._target_precision) / (self._eta_mu + 1)
+            self._lin = self._Q_mu @ self._proj_mean
         except Exception:
             self._succ = False
             self._proj_mean = None
@@ -71,9 +73,11 @@ class SplitMoreProjection(object):
             opt_eta_sig = self.opt_dual(self._dual_cov)
             self._eta_sig = opt_eta_sig
             self._proj_covar = self._new_cov(opt_eta_sig)
+            self._proj_precision = np.linalg.solve(self._proj_covar, np.eye(self._dim))
         except Exception:
             self._succ = False
             self._proj_covar = None
+
         return self._proj_mean, self._proj_covar
 
     def _new_mean(self, eta_mu):
@@ -82,7 +86,8 @@ class SplitMoreProjection(object):
         return np.linalg.solve(mat, vec)
 
     def _new_cov(self, eta_sig):
-        return np.linalg.inv(self._target_precision + eta_sig * self._old_precision) * (eta_sig + 1)
+        return np.linalg.solve(self._target_precision + eta_sig * self._old_precision, np.eye(self._dim)) * (
+                eta_sig + 1)
 
     def _dual_mean(self, eta_mu, grad):
 
@@ -182,43 +187,84 @@ class SplitMoreProjection(object):
         if self._eta_mu > 0:
             at = self.mu_grad_baseline()
             # at, bt, ct, dt = self._case2_tuned()
-            # ab, bb, cb, db = self._case2_baseline()
             # print(np.max(np.abs(cb - ct)), np.max(np.abs(cb - ct)))
 
         if self._eta_sig > 0:  # case 3
             bt = self.sig_grad_baseline()
-            # at, bt, ct, dt = self._case3_baseline2()
-            # ab, bb, cb, db = self._case3_baseline()
             # print(np.max(np.abs(ab - at)), np.max(np.abs(bb - bt)))
 
         return at, bt
 
     def mu_grad_baseline(self):
-        # Q_mu stuff 
-        Q_mu = (self._eta_mu * self._old_precision + self._target_precision) / (self._eta_mu + 1)
-        covar_mu = np.linalg.solve(Q_mu, np.eye(self._dim))
-        dQ_mu_deta_mu = (self._eta_mu * self._old_precision - self._target_precision) / (self._eta_mu + 1) ** 2
+
+        covar_mu = np.linalg.solve(self._Q_mu, np.eye(self._dim))
+        dQ_mu_deta_mu = (self._old_precision - self._target_precision) / (self._eta_mu + 1) ** 2
+        dsig_mu_deta_mu = - covar_mu @ dQ_mu_deta_mu @ covar_mu
+        dq_deta_mu = (self._old_lin - self._target_lin) / (self._eta_mu + 1) ** 2
+
+        dmean_dq = - covar_mu @ self._old_lin + covar_mu @ self._old_precision @ covar_mu @ self._lin
+
+        tmp1 = self._lin @ covar_mu @ dQ_mu_deta_mu @ covar_mu @ self._old_lin
+        tmp2 = self._lin @ dsig_mu_deta_mu @ self._old_precision @ covar_mu @ self._lin
+
+        # TODO: not sure this is faster, no difference in error, though
+        # lin_old = np.outer(self._lin, self._old_lin)
+        # tmp1 = covar_mu @ (0.5 * (lin_old + lin_old.T)) @ covar_mu
+        # lin_lin = self._old_precision @ covar_mu @ np.outer(self._lin, self._lin)
+        # tmp2 = - 0.5 * covar_mu @ (lin_lin + lin_lin.T) @ covar_mu
+        # dcov_dQ_mu = tmp1 + tmp2
+
+        dmean_deta_mu = np.dot(dmean_dq, dq_deta_mu) + tmp1 + tmp2
+        # dmean_deta_mu = np.dot(dmean_dq, dq_deta_mu) + np.trace(dcov_dQ_mu @ dQ_mu_deta_mu)
+
+        lhs = dmean_dq / (self._eta_mu + 1)
+        deta_mu_dq_target = lhs / -dmean_deta_mu
+
+        # return deta_mu_dq_target / (self._eta_mu + 1)
+        return deta_mu_dq_target
+
+    def mu_grad_tuned(self):
+        # Q_mu stuff
+        covar_mu = np.linalg.solve(self._Q_mu, np.eye(self._dim))
+        dQ_mu_deta_mu = (self._old_precision - self._target_precision) / (self._eta_mu + 1) ** 2
         dsig_mu_deta_mu = - covar_mu @ dQ_mu_deta_mu @ covar_mu
 
-        dq_deta_mu = (self._eta_mu * self._old_lin - self._target_lin) / (self._eta_mu + 1) ** 2
-        dmean_dq = - covar_mu @ self._old_lin + covar_mu @ self._old_precision @ covar_mu @ self._target_lin
+        dq_deta_mu = (self._old_lin - self._target_lin) / (self._eta_mu + 1) ** 2
+        dmean_dq = - covar_mu @ self._old_lin + covar_mu @ self._old_precision @ covar_mu @ self._lin
 
-        tmp1 = self._target_lin.T @ covar_mu @ dQ_mu_deta_mu @ covar_mu @ self._old_lin
-        tmp2 = 0.5 * self._target_lin @ dsig_mu_deta_mu @ self._old_precision @ covar_mu @ self._target_lin
-        tmp3 = 0.5 * self._target_lin @ covar_mu @ self._old_precision @ dsig_mu_deta_mu @ self._target_lin
-        dmean_deta_mu = dq_deta_mu @ dmean_dq + tmp1 - tmp2 - tmp3
+        tmp1 = self._lin @ covar_mu @ dQ_mu_deta_mu @ covar_mu @ self._old_lin
+        tmp2 = self._lin @ dsig_mu_deta_mu @ self._old_precision @ covar_mu @ self._lin
+        # tmp2 = 0.5 * self._lin @ dsig_mu_deta_mu @ self._old_precision @ covar_mu @ self._lin
+        # tmp3 = 0.5 * self._lin @ covar_mu @ self._old_precision @ dsig_mu_deta_mu @ self._lin
 
-        deta_mu_dq_target = dmean_dq / dmean_deta_mu
+        # old_cov = np.linalg.inv(self._old_precision)
+        # tmp2 = - self._lin @ self._Q_mu @ old_cov @ self._Q_mu @ old_cov @ self._Q_mu @ dQ_mu_deta_mu @ self._Q_mu @ old_cov @ self._Q_mu @ self._lin
+        dmean_deta_mu = dq_deta_mu @ dmean_dq + tmp1 + tmp2
 
+        lhs = dmean_dq / (self._eta_mu + 1)
+        deta_mu_dq_target = lhs / -dmean_deta_mu
+
+        # return deta_mu_dq_target / (self._eta_mu + 1)
         return deta_mu_dq_target
 
     def sig_grad_baseline(self):
-        dQ_deta_sig = (self._eta_sig * self._old_precision - self._target_precision) / (self._eta_sig + 1) ** 2
+        dQ_deta_sig = (self._old_precision - self._target_precision) / (self._eta_sig + 1) ** 2
+        dcov_dQ = 0.5 * (-self._proj_covar @ self._old_precision @ self._proj_covar + self._proj_covar)
         dcov_deta_sig = 0.5 * np.trace(
-            -self._old_precision @ self._proj_covar @ dQ_deta_sig @ self._proj_covar + self._proj_covar @ dQ_deta_sig)
-        dcov_dQ = 0.5 * (-self._proj_covar @ self._old_precision @ self._proj_covar + self._old_precision)
+            (-self._proj_covar @ self._old_precision @ self._proj_covar + self._proj_covar) @ dQ_deta_sig)
 
-        deta_sig_dQ_target = dcov_dQ / dcov_deta_sig
+        lhs = dcov_dQ / (self._eta_sig + 1)
+        deta_sig_dQ_target = lhs / -dcov_deta_sig
+
+        return deta_sig_dQ_target
+
+    def sig_grad_tuned(self):
+        dQ_deta_sig = (self._old_precision - self._proj_precision) / (self._eta_sig + 1)
+        dcov_dQ = -self._proj_covar @ self._old_precision @ self._proj_covar + self._proj_covar
+        dcov_deta_sig = np.trace(dcov_dQ @ dQ_deta_sig)
+
+        lhs = dcov_dQ / (self._eta_sig + 1)
+        deta_sig_dQ_target = lhs / -dcov_deta_sig
 
         return deta_sig_dQ_target
 

python/tests/ecc_backward.py
@@ -1,15 +1,11 @@
-import numpy as np
-from util.Gaussian import Gaussian
-from util.central_differences import central_differences
-from util.sample import sample_sympd
 import cpp_projection as proj
-from cvx.cvxlayers_projection import CVXProjector
+import numpy as np
 import tensorflow as tf
 from tensorflow_probability import distributions as tfd
 
-from projection.pytorch_op import ProjectionSimpleNet
-from torch.distributions import MultivariateNormal
-import torch
+from python.util.Gaussian import Gaussian
+from python.util.central_differences import central_differences
+from python.util.sample import sample_sympd
 
 """
 Regression test for backward pass against numerical approximation and cvxopt
@@ -32,7 +28,8 @@ q_old = Gaussian(mean_old, cov_old)
 q_target = Gaussian(mean_target, cov_target)
 samples = q_target.sample(10)
 
-proj_more = proj.MoreProjection(dim, eec=True, constrain_entropy=True)
+# proj_more = proj.MoreProjection(dim, eec=True, constrain_entropy=True)
+proj_more = proj.MoreProjection(dim, eec=True)
 
 def sym_wrapper(x):
     x = np.reshape(x, [dim, dim])

python/tests/test_split_grad.py
@@ -10,7 +10,7 @@ from python.util.sample import sample_sympd
 diff_delta = 1e-4  # delta for the central different gradient approximator, the whole thing is kind of sensitve to that
 
 np.random.seed(0)
-dim = 1
+dim = 20
 
 mean_old = np.random.uniform(low=-1, high=1, size=dim)
 cov_old = sample_sympd(dim)
@@ -22,6 +22,8 @@ q_old = Gaussian(mean_old, cov_old)
 q_target = Gaussian(mean_target, cov_target)
 
 proj_more = SplitMoreProjection(dim)
+
+p0 = np.concatenate([q_target.lin_term, np.reshape(q_target.precision, [-1])], axis=-1)
 
 
 def eval_fn(p, eps_mu, eps_sigma):
@@ -37,9 +39,6 @@ def eval_fn(p, eps_mu, eps_sigma):
     return np.array([proj_more.last_eta_mu, proj_more.last_eta_sig])
 
 
-p0 = np.concatenate([q_target.lin_term, np.reshape(q_target.precision, [-1])], axis=-1)
-
-
 def run_test(eps_mu, eps_sigma):
     _, _ = proj_more.more_step(eps_mu, eps_sigma, q_old.mean, q_old.covar, q_target.mean, q_target.covar)
     # deq, deQ = proj_more.backward(np.zeros((dim,)), np.zeros((dim, dim)))
@@ -52,24 +51,30 @@ def run_test(eps_mu, eps_sigma):
     # print("d eta_mu d q")
     # print("analytical", deq)
     # print("numerical", numerical_grads[0, :dim])
-    # print("fraction", numerical_grads[0, :dim] / deq)
-    print("max diff  d_eta_mu d_q", np.max(np.abs(numerical_grads[0, :dim] - deq)))
+    # if np.any(deq != 0):
+    #     print("deq fraction", deq / numerical_grads[0, :dim])
+    diff_q = numerical_grads[0, :dim] - deq
+    print("max diff  d_eta_mu d_q", np.max(np.abs(diff_q)))
+    print("mean diff  d_eta_mu d_q", np.mean(diff_q))
 
     # print("d eta_mu d Q")
     # print("analytical", deQ)
     # print("numerical", np.reshape(numerical_grads[0, dim:], [dim, dim]))
-    # print("fraction", np.reshape(numerical_grads[0, dim:], [dim, dim]) / deQ)
-    print("max diff d_eta_sigma d_Q", np.max(np.abs(np.reshape(numerical_grads[0, dim:], [dim, dim]) - deQ)))
+    # if np.any(deQ != 0):
+    #     print("deQ fraction", deQ / np.reshape(numerical_grads[1, dim:], [dim, dim]))
+    diff_Q = np.reshape(numerical_grads[1, dim:], [dim, dim]) - deQ
+    print("max diff d_eta_sigma d_Q", np.max(np.abs(diff_Q)))
+    print("mean diff d_eta_sigma d_Q", np.mean(diff_Q))
 
 
 print("--------BOTH INACTIVE------------------")
-run_test(eps_mu=10.0, eps_sigma=10.0)
+run_test(eps_mu=100.0, eps_sigma=100.0)
 
 print("--------MEAN ACTIVE------------------")
-run_test(eps_mu=0.01, eps_sigma=10.0)
+run_test(eps_mu=0.01, eps_sigma=100.0)
 
 print("--------COVARIANCE ACTIVE------------------")
-run_test(eps_mu=10.0, eps_sigma=0.01)
+run_test(eps_mu=100.0, eps_sigma=0.01)
 
 print("--------BOTH ACTIVE------------------")
 run_test(eps_mu=0.01, eps_sigma=0.001)